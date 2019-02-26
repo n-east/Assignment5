79dd7e39e3006806092bfa0784aff42c659096bf
Noelle East <neast@student.waubonsee.edu>
2019-02-26 11:20:15 -0600

    I have no idea what changes I made but I presume that I did something.

--- assignment5.py
+++ assignment5.py
@@ -5,9 +5,7 @@ print(second)
 if int(first) < 0 or int(second) < 0:
                  print("You need to enter a POSITIVE number.")
 else:
-    print("The sum of the numbers your entered is: ", (int(first) + int(second)))
+    print("The sum of the numbers you entered is: ", (int(first) + int(second)))
     print("The result of subtracting", second, "from", first, "is", (int(first) - int(second)))
     print("The result of multiplying the numbers you entered is ", (int(first) * int(second)))
     print("The result of dividing", first, "by", second, "is", (int(first)/int(second)))
-                 
-
