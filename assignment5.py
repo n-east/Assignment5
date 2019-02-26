4f4f81a9c6809e99a231e22d5852cc3a7fa20c37
Noelle East <neast@student.waubonsee.edu>
2019-02-26 11:06:07 -0600

    Begin simple program that can add/subtract two numbers.

---
+++ assignment5.py
@@ -0,0 +1,11 @@
+first = input("Enter one positive number: ")
+print(first)
+second = input("Enter a second positive number: ")
+print(second)
+if int(first) < 0 or int(second) < 0:
+                 print("You need to enter a POSITIVE number.")
+else:
+    print("The sum of the numbers your entered is: ", (int(first) + int(second)))
+    print("The result of subtracting", second, "from", first, "is", (int(first) - int(second)))
+                 
+
